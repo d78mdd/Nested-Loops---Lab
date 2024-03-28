ac6abb66e619941e7c2eec626295282033942203
dkostadinov <d78mdd@gmail.com>
2024-03-28 23:29:30 +0200

    Add project files.

---
+++ 02. Multiplication Table/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02.Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int m1 = 1; m1 <= 10; m1++)
+            {
+                for (int m2 = 1; m2 <= 10; m2++)
+                {
+                    Console.WriteLine($"{m1} * {m2} = {m1 * m2}");
+                }
+            }
+        }
+    }
+}
---
+++ 04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _04.Sum_of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nachaloNaIntervala = int.Parse(Console.ReadLine());
+            int kraiNaIntervala = int.Parse(Console.ReadLine());
+            int magicheskoChislo = int.Parse(Console.ReadLine());
+
+            int broiNaVsichkiKombinacii = 0;
+            int poredenNomer = 0;
+            int parvoChislo = 0;
+            int vtoroChislo = 0;
+
+            for (int x1 = nachaloNaIntervala; x1 <= kraiNaIntervala; x1++)
+            {
+                for (int x2 = nachaloNaIntervala; x2 <= kraiNaIntervala; x2++)
+                {
+                    broiNaVsichkiKombinacii++;
+                    if (x1 + x2 == magicheskoChislo)
+                    {
+                        poredenNomer = broiNaVsichkiKombinacii;
+                        parvoChislo = x1;
+                        vtoroChislo = x2;
+                        break;
+                    }
+                }
+                if (poredenNomer != 0)
+                {
+                    break;
+                }
+            }
+
+            if (poredenNomer != 0)
+            {
+                Console.WriteLine($"Combination N:{poredenNomer} ({parvoChislo} + {vtoroChislo} = {magicheskoChislo})");
+            }
+            else
+            {
+                Console.WriteLine($"{broiNaVsichkiKombinacii} combinations - neither equals {magicheskoChislo}");
+            }
+        }
+    }
+}
