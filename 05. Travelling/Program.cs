c4c6bee9bb00566d2345707adbca6ad7360e130f
dkostadinov <d78mdd@gmail.com>
2024-03-29 22:20:04 +0200

    add solutions

---
+++ 05. Travelling/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05.Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            do
+            {
+                double spesteni = 0;
+
+                input = Console.ReadLine();
+
+                string destinacia;
+                if (input != "End")
+                {
+                    destinacia = input;
+                }
+                else
+                {
+                    break;
+                }
+
+
+                double minimalenBiodzhet = double.Parse(Console.ReadLine());
+
+                do
+                {
+                    spesteni += double.Parse(Console.ReadLine());
+                }
+                while (spesteni < minimalenBiodzhet);
+
+                Console.WriteLine($"Going to {destinacia}!");
+
+            } while (input != "End");
+
+        }
+    }
+}
---
+++ 06. Building/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _06.Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broiEtaji = int.Parse(Console.ReadLine());
+            int broiStai = int.Parse(Console.ReadLine());
+
+            for (int nomerNaEtaj = broiEtaji; nomerNaEtaj > 0; nomerNaEtaj--)
+            {
+                bool cheten;
+
+                if (nomerNaEtaj % 2 == 0)
+                {
+                    cheten = true;
+                }
+                else
+                {
+                    cheten = false;
+                }
+
+                char tipStaia;
+
+                if (nomerNaEtaj == broiEtaji)
+                {
+                    tipStaia = 'L';
+                }
+                else
+                {
+                    if (cheten)
+                    {
+                        tipStaia = 'O';
+                    }
+                    else
+                    {
+                        tipStaia = 'A';
+                    }
+                }
+
+                for (int nomerNaStaia = 0; nomerNaStaia < broiStai; nomerNaStaia++)  // apartament, ofis, large apartament
+                {
+                    Console.Write($"{tipStaia}{nomerNaEtaj}{nomerNaStaia} ");
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
